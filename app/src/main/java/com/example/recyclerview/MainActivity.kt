e8645f629349bc7bb1636bfbf10e00501655373e
Basma-Alqethami <91012377+Basma-Alqethami@users.noreply.github.com>
2021-11-28 08:13:54 +0300

    done

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var submitButton: Button
     private lateinit var messageText: TextView
     private val messages = arrayListOf<String>()
+    private lateinit var clMain: ConstraintLayout
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,15 +26,23 @@ class MainActivity : AppCompatActivity() {
         myRV = findViewById<RecyclerView>(R.id.rvMain)
         submitButton = findViewById<Button>(R.id.submitButton)
         messageText = findViewById<TextView>(R.id.messageText)
+        clMain = findViewById(R.id.clMain)
+
+        myRV.adapter = RecyclerViewAdapter(messages)
+        myRV.layoutManager = LinearLayoutManager(this)
 
         submitButton.setOnClickListener { AddTOList() }
     }
 
     private fun AddTOList(){
         var message = messageText.text.toString()
-        messages.add(message)
-        myRV.adapter = RecyclerViewAdapter(messages)
-        myRV.layoutManager = LinearLayoutManager(this)
-        messageText.text = ""
+
+        if(message.isNotEmpty()){
+            messages.add(message)
+            myRV.adapter!!.notifyDataSetChanged()
+            messageText.text = ""
+        }else{
+            Snackbar.make(clMain, "Please enter some text", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
